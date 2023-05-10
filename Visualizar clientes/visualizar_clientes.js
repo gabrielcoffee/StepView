654138f0dea637a9669ecf2900b67267f09704ea
AllesBlaw <103613091+NickSigler@users.noreply.github.com>
2023-05-10 10:27:08 -0300

    Atualização na tela de visu

---
+++ Visualizar clientes/visualizar_clientes.js
@@ -0,0 +1,16 @@
+const tela = document.querySelector(".tela_cadastro")
+
+
+function abrirModal()
+{
+    tela.classList.add("ola")
+}
+
+function sairModal()
+{
+    tela.classList.remove("ola")
+}
+
+
+
+console.log(tela);
