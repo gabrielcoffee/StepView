25fb68028fd6c412184a24bb50fd0bde877100e2
AllesBlaw <103613091+NickSigler@users.noreply.github.com>
2023-05-14 10:04:38 -0300

    Update PHPconfig.php

--- CORPOS/PHPconfig.php
+++ CORPOS/PHPconfig.php
@@ -1,10 +1,12 @@
 <?php
 
-// Em casa (Gabriel)
+/* Em casa (Gabriel)
 $server_name = 'localhost';
-$username = 'root';
-$password = '';
+$username = 'admin';
+$password = 'admin';
 $database = 'stepview';
+*/
+
 
 /*
 // Na Puc
@@ -13,6 +15,14 @@ $username = 'root';
 $password = '';
 $database = 'stepview';
 */
+
+// Em casa (Nicolas
+$server_name = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'stepview';
+
+
 
 $conn = new mysqli($server_name, $username, $password, $database);
 
