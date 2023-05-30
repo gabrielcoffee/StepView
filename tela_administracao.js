f93f28890effd5f6afa2b577be19e12c6ed4b467
AllesBlaw <103613091+NickSigler@users.noreply.github.com>
2023-05-30 20:11:30 -0300

    asdas

---
+++ tela_administracao.js
@@ -0,0 +1,11 @@
+const tela = document.querySelector(".telaModal");
+
+function abrirModal()
+{
+    tela.classList.add("ola");
+}
+
+function sairModal()
+{
+    tela.classList.remove("ola");
+}
---
+++ tela_administracao.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="tela_administracao.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="telaModal">
+        <div class="botaoSair">
+            <button onclick="sairModal()" id="botaoSair">X</button>
+        </div>
+        <div class="containerModal">
+            <div class="titulo">
+                
+            </div>
+            <div class="containerForm">
+                <div class="formOdonto">
+                    <h1>Adicionar Odontologo</h1>
+                    <form action="" method="">
+                        <div>
+                            <input name="nome" type="text" placeholder="Nome">
+                        </div>
+                        <div>
+                            <input name="crm" type="text" placeholder="Crm">
+                        </div>
+                     
+                        <div>
+                            <input name="senha" type="password" placeholder="Senha">
+                        </div>
+                        <div>
+                            <input name="senha" type="password" placeholder="Senha">
+                        </div>
+                        <div class="botoes">
+                            <button>Registrar</button>
+                        </div>
+
+
+
+                        
+
+                    </form>
+                </div>
+                <div class="formSecre">
+
+                    <div class="titulo">
+                        <h1>Registrar Funcionario</h1>
+                    </div>
+                    <form action="" method="">
+                        <div>
+                            <input name="nome" type="text" placeholder="Nome">
+                        </div>
+                        <div>
+                            <input name="crm" type="text" placeholder="Crm">
+                        </div>
+                     
+                        <div>
+                            <input name="senha" type="password" placeholder="Senha">
+                        </div>
+                        <div>
+                            <input name="senha" type="password" placeholder="Senha">
+                        </div>
+                        <div class="botoes">
+                            <button>Registrar</button>
+                        </div>
+
+
+
+                        
+
+                    </form>
+                </div>
+            </div>
+
+        </div>
+
+
+    </div>
+    <div class="container">
+
+    
+        <header>
+            <h3>Seja bem vindo!</h3>
+        </header>
+        <nav>
+            <button>Visualizar Estados</button>
+            <button>Visualizar Clientes</button>
+            <button onclick="abrirModal()" class="botaoCriar">
+                Registrar funcionario
+                </button>
+        </nav>
+        
+        <main>
+        
+        </main>
+        <aside class="assideEsquerdo">
+            <h1>StepView</h1>
+        </aside>
+        <aside class="assideDireito">
+            <table>
+                
+       
+            </table>
+        </aside>
+        <footer>
+            <h1>footer</h1>
+        </footer>
+    </div>
+    <script src="tela_administracao.js"></script>
+</body>
+</html>
