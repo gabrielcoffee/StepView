0dc24f141348cb48a6baa740678bb0a80c945a95
AllesBlaw <103613091+NickSigler@users.noreply.github.com>
2023-06-01 19:59:20 -0300

    att

---
+++ Novos Designs/visualizar_clientes.js
@@ -0,0 +1,34 @@
+const tela = document.querySelector(".telaModal");
+const telaDeletar = document.querySelector(".tela_deletar");
+
+function abrirModal()
+{
+    tela.classList.add("ola");
+}
+
+function abrirConfirmarDeletar(cpf)
+{
+    if (window.confirm("Realmente deseja deletar cliente?")) {
+        location.href = "PHPdeletar_cliente.php?cpf=" + cpf;
+    }
+    
+}
+
+function recarregarPagina()
+{
+    location.href = "visualizar_clientes.php";
+}
+
+function carregarPaginaCpf(cpf)
+{
+    location.href = "visualizar_clientes.php?cpf=" + cpf;
+}
+
+function sairModal()
+{
+    tela.classList.remove("ola");
+    telaDeletar.classList.remove("ola");
+}
+
+
+console.log(tela);
