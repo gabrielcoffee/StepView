17a1074b82abf44ac003b95cbeb863d19c767914
AllesBlaw <107810480+Brokolizz@users.noreply.github.com>
2023-06-02 23:00:35 -0300

    Update registrar_funcionario.php

--- CORPOS/registrar_funcionario.php
+++ CORPOS/registrar_funcionario.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="PT-BR">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../ESTILOS/registro_funcionario.css">
-    <title>StepView Registro</title>
+    <title>SStepView</title>
 </head>
 <body>
    <div class="lateral_esquerdo">
